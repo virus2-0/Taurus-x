149c165bfe19f6a8403f0f0e5a2d5903f3e7ea9b
virus2-0 <98601478+virus2-0@users.noreply.github.com>
2022-01-30 13:43:01 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -6,13 +6,13 @@ if (Config.WORKTYPE == 'private') {
 
     Julie.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '15369524516-1612300121@g.us') {
+        if (message.jid === '917023951514-1612300121@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*TAURUS BOT BY MUHAMMED*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*TAURUS BOT BY Professor*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
@@ -24,13 +24,13 @@ else if (Config.WORKTYPE == 'public') {
 
     Julie.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '54218542512-1612300121@g.us') {
+        if (message.jid === '917023951514-1612300121@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*TAURUS BOT BY MUHAMMED*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*TAURUS BOT BY Professor*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
